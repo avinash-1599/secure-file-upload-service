4666e7b73ecfcc33878c6d76ea6194aff082cd5b
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2025-06-05 05:52:25 +0530

    auth controller updated

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -34,19 +34,4 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// Auth middleware
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
 module.exports = router;
-module.exports.authenticateJWT = authenticateJWT;
